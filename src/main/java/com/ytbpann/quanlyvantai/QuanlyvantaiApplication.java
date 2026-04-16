b134fa78a1684a078243ed9198338d3ad1032051
Phuoc Nguyen Hoang <himari.fb@gmail.com>
2026-04-17 03:10:59 +0700

    ...

---
+++ src/main/java/com/ytbpann/quanlyvantai/QuanlyvantaiApplication.java
@@ -0,0 +1,13 @@
+package com.ytbpann.quanlyvantai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlyvantaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanlyvantaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ytbpann/quanlyvantai/QuanlyvantaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ytbpann.quanlyvantai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanlyvantaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
